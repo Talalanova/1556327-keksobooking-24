b89381546fcfd34d410e0b7ff3cb956c83cad1bd
Talanova <pro_ego@mail.ru>
2021-11-06 17:04:58 +1000

    фильтры

--- js/main.js
+++ js/main.js
@@ -2,9 +2,13 @@ import {renderMarkers} from './map-set.js';
 import {setUserFormSubmit} from './advertisment-form.js';
 import {getData} from './api.js';
 import {getAdTemplate} from './draw-advertisment.js';
+import {typeCLick, guestsClick, roomsClick} from './map-set.js';
 
 getData((advertisments) => {
   renderMarkers(advertisments,getAdTemplate);
+  typeCLick(() => renderMarkers(advertisments,getAdTemplate));
+  guestsClick(() => renderMarkers(advertisments,getAdTemplate));
+  roomsClick(() => renderMarkers(advertisments,getAdTemplate));
 });
 
 
--- js/map-set.js
+++ js/map-set.js
@@ -1,11 +1,5 @@
 import {makeMap} from './make-map-object.js';
 
-const typeInput = document.querySelector('#housing-type');
-
-typeInput.addEventListener('change', () => {
-
-});
-
 const TOKIO_CENTER = {
   lat: 35.4100,
   lng: 139.4130,
@@ -13,9 +7,46 @@ const TOKIO_CENTER = {
 
 const map = makeMap(TOKIO_CENTER);
 
+const typeInput = document.querySelector('#housing-type');
+let selectedType = typeInput.value;
+
+const roomsInput = document.querySelector('#housing-rooms');
+let selectedRooms = roomsInput.value;
+
+const guestsInput = document.querySelector('#housing-guests');
+let selectedGuests = guestsInput.value;
+
 const renderMarkers = (advertisments,template) => {
-  const filteredAds = advertisments.filter((ad) => ad.offer.type === typeInput.value);
+  const filteredAds = advertisments.filter((ad) => (
+    (selectedType === 'any' || ad.offer.type === selectedType) &&
+    (selectedGuests === 'any' || ad.offer.guests === selectedGuests) &&
+    (selectedRooms === 'any' || ad.offer.rooms === selectedRooms)
+  ));
   map.setSecondaryMarkers(filteredAds,template);
+};
+
+export const typeCLick = (cb) => {
+  typeInput.addEventListener('input', () => {
+    map.clearMarkers();
+    selectedType = typeInput.value;
+    cb();
+  });
+};
+
+export const roomsClick = (cb) => {
+  roomsInput.addEventListener('input', () => {
+    map.clearMarkers();
+    selectedRooms = roomsInput.value;
+    cb();
+  });
+};
+
+export const guestsClick = (cb) => {
+  guestsInput.addEventListener('input', () => {
+    map.clearMarkers();
+    selectedGuests = guestsInput.value;
+    cb();
+  });
 };
 
 export {map,renderMarkers,TOKIO_CENTER};
